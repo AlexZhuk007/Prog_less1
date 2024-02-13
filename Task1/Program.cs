01c254850ee2487c3efa279658b33be69020aecc
Alex Zhuk <89257358590@mail.ru>
2024-02-13 17:02:56 +0300

    Добавили некоторые переменные

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.WriteLine ("Жопа");
-12
+﻿// Console.WriteLine ("Жопа");
+
+
+int value = 100;
+Console.WriteLine (value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine (d);
+Console.WriteLine (c);
+Console.WriteLine (b);
