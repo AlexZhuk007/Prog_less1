8e83ba6291b3d38730d074fbf121787c27fc3883
Alex Zhuk <89257358590@mail.ru>
2024-02-13 20:09:09 +0300

    удалил ссылки в конфиге

--- Task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+118893723b1a3571826ad25c21fbfb2bcd15aa4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a6424fa5656e81e76ef2197d8112e8ae75b180")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
