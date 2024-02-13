f059bd0b97bb3e41fd4616b36699a7614092b3ff
Alex Zhuk <89257358590@mail.ru>
2024-02-13 22:25:19 +0300

    homeworkСд

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.WriteLine ("Жопа (_|_)");
+﻿//Console.WriteLine ("Жопа (_|_)");
 
 
-int value = 100;
-Console.WriteLine(value);
+//int value = 100;
+//Console.WriteLine(value);
 
 // double d = 5.55;
 // char c = '!';
@@ -11,3 +11,10 @@ Console.WriteLine(value);
 // Console.WriteLine (c);
 // Console.WriteLine (b);
 
+float a = 9999999999999999999;
+float b = 9999999999999999999;
+float c = 9999999999999999999;
+float d = 9999999999999999999;
+
+float rez = (a*b)/(c+d);
+Console.WriteLine(rez);
--- Task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a6424fa5656e81e76ef2197d8112e8ae75b180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e83ba6291b3d38730d074fbf121787c27fc3883")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
